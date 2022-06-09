2c6660e4416aa0aa5ed7a7f29b416f2aa4a75e52
Jonas Claes <jonas@jonasclaes.be>
2022-06-09 17:42:13 +0200

    BotService tests

--- apps/discordbot/src/discord/bot/bot/bot.service.spec.ts
+++ apps/discordbot/src/discord/bot/bot/bot.service.spec.ts
@@ -1,21 +1,98 @@
 import { Guild } from '@common/common/guild/guild.entity';
+import { Provider } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CommandService } from '../../command/command.service';
 import { BotService } from './bot.service';
 
 describe('BotService', () => {
-  let service: BotService;
+  let botService: BotService;
+  let commandService: CommandService;
+
+  let mockGuilds: Guild[] = [];
+
+  const mockGuildRepository = {
+    find: jest.fn(),
+    findOneBy: jest
+      .fn()
+      .mockImplementation((data: { id: number }) =>
+        mockGuilds.find((g) => g.id === data.id),
+      ),
+  };
+
+  const mockedCommandService: Provider = {
+    provide: CommandService,
+    useFactory: () => ({
+      putGuildsCommands: jest.fn(),
+      putGuildCommands: jest.fn(),
+    }),
+  };
 
   beforeEach(async () => {
-    // const module: TestingModule = await Test.createTestingModule({
-    //   imports: [TypeOrmModule.forFeature([Guild])],
-    //   providers: [BotService, CommandService],
-    // }).compile();
-    // service = module.get<BotService>(BotService);
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BotService,
+        mockedCommandService,
+        {
+          provide: getRepositoryToken(Guild),
+          useValue: mockGuildRepository,
+        },
+      ],
+    }).compile();
+
+    botService = module.get<BotService>(BotService);
+    commandService = module.get<CommandService>(CommandService);
+
+    mockGuilds = [];
+
+    const guild = new Guild();
+    guild.id = 1;
+    guild.name = 'test';
+    guild.snowflake = '123456789';
+    mockGuilds.push(guild);
   });
 
   it('should be defined', () => {
-    // expect(service).toBeDefined();
+    expect(botService).toBeDefined();
+  });
+
+  describe('loadAllGuildsCommands', () => {
+    it('should return', async () => {
+      jest.spyOn(commandService, 'putGuildsCommands').mockResolvedValue();
+
+      expect(botService.loadAllGuildsCommands()).resolves.toBeUndefined();
+    });
+
+    it('should throw an error', async () => {
+      jest
+        .spyOn(commandService, 'putGuildsCommands')
+        .mockRejectedValue(new Error());
+
+      await expect(botService.loadAllGuildsCommands()).rejects.toThrow();
+    });
+  });
+
+  describe('loadGuildCommands', () => {
+    it('should return', async () => {
+      jest.spyOn(commandService, 'putGuildCommands').mockResolvedValue();
+
+      await expect(botService.loadGuildCommands(1)).resolves.toBeUndefined();
+      expect(commandService.putGuildCommands).toHaveBeenCalledWith(
+        mockGuilds[0],
+      );
+    });
+
+    it('should throw an error', async () => {
+      jest
+        .spyOn(commandService, 'putGuildCommands')
+        .mockRejectedValue(new Error());
+
+      await expect(botService.loadGuildCommands(1)).rejects.toThrow();
+    });
+
+    it('should throw a not found error', async () => {
+      await expect(botService.loadGuildCommands(2)).rejects.toThrow();
+    });
   });
 });
