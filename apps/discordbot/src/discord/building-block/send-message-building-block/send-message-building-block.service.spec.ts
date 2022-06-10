242884652670de0e9568026b65ca7d67bc79882e
Jonas Claes <jonas@jonasclaes.be>
2022-06-10 19:47:41 +0200

    Send message building block tests

--- apps/discordbot/src/discord/building-block/send-message-building-block/send-message-building-block.service.spec.ts
+++ apps/discordbot/src/discord/building-block/send-message-building-block/send-message-building-block.service.spec.ts
@@ -1,21 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import { Discord } from '../../discord';
-import { SendMessageBuildingBlockService } from './send-message-building-block.service';
+import {
+  SendMessageBuildingBlockService,
+  SendMessageTo,
+} from './send-message-building-block.service';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('SendMessageBuildingBlockService', () => {
-  let service: SendMessageBuildingBlockService;
+  let sendMessageBuildingBlockService: SendMessageBuildingBlockService;
+  let mockedDiscord: Discord;
 
   beforeEach(async () => {
-    // const module: TestingModule = await Test.createTestingModule({
-    //   imports: [Discord],
-    //   providers: [SendMessageBuildingBlockService],
-    // }).compile();
-    // service = module.get<SendMessageBuildingBlockService>(
-    //   SendMessageBuildingBlockService,
-    // );
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SendMessageBuildingBlockService],
+    })
+      .useMocker((token) => {
+        if (token === Discord) {
+          const _mockUsers = {};
+
+          return {
+            users: {
+              fetch: jest.fn().mockImplementation(() => Promise.resolve()),
+              cache: {
+                get: jest.fn().mockImplementation((id) => {
+                  if (!_mockUsers[id])
+                    _mockUsers[id] = {
+                      id,
+                      send: jest
+                        .fn()
+                        .mockImplementation(() => Promise.resolve()),
+                    };
+                  return _mockUsers[id];
+                }),
+              },
+            },
+          };
+        }
+
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
+
+    sendMessageBuildingBlockService =
+      module.get<SendMessageBuildingBlockService>(
+        SendMessageBuildingBlockService,
+      );
+    mockedDiscord = module.get<Discord>(Discord);
   });
 
   it('should be defined', () => {
-    // expect(service).toBeDefined();
+    expect(sendMessageBuildingBlockService).toBeDefined();
+  });
+
+  describe('handle', () => {
+    it('should send a message', async () => {
+      const userSnowflake = '1';
+
+      await sendMessageBuildingBlockService.handle({
+        toType: SendMessageTo.USER,
+        to: userSnowflake,
+        messageFormat: 'Hello {{user}}',
+        messageParameters: {
+          user: 'John',
+        },
+      });
+
+      expect(
+        mockedDiscord.users.cache.get(userSnowflake).send,
+      ).toHaveBeenCalledWith('Hello John');
+    });
   });
 });
