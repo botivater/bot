6c978cb8eca11d5e1d4256354917edccdbf0ac5f
Jonas Claes <jonas@jonasclaes.be>
2022-06-10 14:10:30 +0200

    Removed unit test of add role building block service

--- apps/discordbot/src/discord/building-block/add-role-building-block/add-role-building-block.service.spec.ts
+++ apps/discordbot/src/discord/building-block/add-role-building-block/add-role-building-block.service.spec.ts
@@ -2,18 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AddRoleBuildingBlockService } from './add-role-building-block.service';
 
 describe('AddRoleBuildingBlockService', () => {
-  let service: AddRoleBuildingBlockService;
+  let addRoleBuildingBlockService: AddRoleBuildingBlockService;
 
   beforeEach(async () => {
     // const module: TestingModule = await Test.createTestingModule({
     //   providers: [AddRoleBuildingBlockService],
     // }).compile();
-    // service = module.get<AddRoleBuildingBlockService>(
+    // module.useLogger(false);
+    // addRoleBuildingBlockService = module.get<AddRoleBuildingBlockService>(
     //   AddRoleBuildingBlockService,
     // );
   });
 
   it('should be defined', () => {
-    // expect(service).toBeDefined();
+    // expect(addRoleBuildingBlockService).toBeDefined();
   });
 });
