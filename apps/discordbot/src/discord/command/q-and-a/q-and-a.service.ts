8684ddb82531b676d75a0b57078c4b68287cc973
Jonas Claes <jonas@jonasclaes.be>
2022-06-16 18:42:54 +0200

    Improve OpenAI

--- apps/discordbot/src/discord/command/q-and-a/q-and-a.service.ts
+++ apps/discordbot/src/discord/command/q-and-a/q-and-a.service.ts
@@ -79,7 +79,31 @@ export class QAndAService implements Command {
 
       const response = await this.openAiApi.createCompletion({
         model: 'text-curie-001',
-        prompt: `Answer in Dutch. Q: ${question}\nA:`,
+        prompt: `
+        I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".
+
+        Q: What is human life expectancy in the United States?
+        A: Human life expectancy in the United States is 78 years.
+
+        Q: Who was president of the United States in 1955?
+        A: Dwight D. Eisenhower was president of the United States in 1955.
+
+        Q: Which party did he belong to?
+        A: He belonged to the Republican Party.
+
+        Q: What is the square root of banana?
+        A: Unknown
+
+        Q: How does a telescope work?
+        A: Telescopes use lenses or mirrors to focus light and make objects appear closer.
+
+        Q: Where were the 1992 Olympics held?
+        A: The 1992 Olympics were held in Barcelona, Spain.
+
+        Q: How many squigs are in a bonk?
+        A: Unknown
+
+        Q: ${question}\nA:`,
         temperature: 0,
         max_tokens: 150,
         top_p: 1,
