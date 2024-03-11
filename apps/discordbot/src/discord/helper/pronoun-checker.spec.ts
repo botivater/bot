0cc009cb759126142d369f0231e4e08f6d205469
MerijnBubble <merijnboot@gmail.com>
2024-03-11 21:43:13 +0100

    Fix-pronounChecker
    Testgevallen en fix toegevoegd

--- apps/discordbot/src/discord/helper/pronoun-checker.spec.ts
+++ apps/discordbot/src/discord/helper/pronoun-checker.spec.ts
@@ -4,4 +4,33 @@ describe('PronounChecker', () => {
   it('should be defined', () => {
     expect(new PronounChecker()).toBeDefined();
   });
+
+  it('Merijn - hij/hem', () => {
+    expect(PronounChecker.checkString('Merijn - hij/hem') ).toBe(true);
+  });
+
+  it('Merijn - hji/hem', () => {
+    expect(PronounChecker.checkString('Merijn - hji/hem') ).toBe(false);
+  });
+
+  it('Merijn - die/diezens', () => {
+    expect(PronounChecker.checkString('Merijn - die/diezens') ).toBe(false);
+  });
+
+  it('Merijn', () => {
+    expect(PronounChecker.checkString('Merijn') ).toBe(false);
+  });
+
+  it('Merijn - E', () => {
+    expect(PronounChecker.checkString('Merijn - E') ).toBe(false);
+  });
+ 
+  it('E', () => {
+    expect(PronounChecker.checkString('E') ).toBe(false);
+  }); 
+
+  it('', () => {
+    expect(PronounChecker.checkString('') ).toBe(false);
+  }); 
+
 });
